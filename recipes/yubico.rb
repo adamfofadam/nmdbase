cde169b940f953b8399b9123e1a6d7282a2b6450
Kevin Bridges <kevin@cyberswat.com>
2014-03-25 17:51:49 -0600

    Attempting to address foodcritic issues.

--- recipes/yubico.rb
+++ recipes/yubico.rb
@@ -25,7 +25,7 @@ when 'debian', 'ubuntu'
   end
 end
 
-template node[:base][:yubico][:authfile] do
+template node['base']['yubico']['authfile'] do
   source "yubikey_mappings.erb"
   mode 0644
   owner "root"
@@ -34,7 +34,7 @@ end
 
 bash "Enable pam_yubico" do
   user "root"
-  cmd = "echo -e \"auth required pam_yubico.so mode=client try_first_pass id=#{node[:base][:yubico][:id]} key=#{node[:base][:yubico][:key]} authfile=#{node[:base][:yubico][:authfile]}\n$(cat /etc/pam.d/sshd)\" > /etc/pam.d/sshd"
+  cmd = "echo -e \"auth required pam_yubico.so mode=client try_first_pass id=#{node['base']['yubico']['id']} key=#{node['base']['yubico']['key']} authfile=#{node['base']['yubico']['authfile']}\n$(cat /etc/pam.d/sshd)\" > /etc/pam.d/sshd"
   not_if "grep 'auth required pam_yubico.so' /etc/pam.d/sshd"
   code <<-EOH
     #{cmd}
