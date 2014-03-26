ad0933a43e203868790108ed876c14e136a27efe
Kevin Bridges <kevin@cyberswat.com>
2014-03-25 20:04:10 -0600

    Fixing a few major errors.

--- spec/chefclient_spec.rb
+++ spec/chefclient_spec.rb
@@ -5,11 +5,13 @@ describe "base::default" do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
   it "logs a sample message" do
-    expect(chef_run).to write_log "replace this with a meaningful resource"
+    expect(:chef_run).to write_log "replace this with a meaningful resource"
   end
 
-  expect(chef_run).to include_recipe('chef-client::delete_validation')
-  expect(chef_run).to include_recipe('chef-client::config')
-  expect(chef_run).to include_recipe('chef-client::service')
+  it 'does something' do
+    expect(chef_run).to include_recipe('chef-client::delete_validation')
+    expect(chef_run).to include_recipe('chef-client::config')
+    expect(chef_run).to include_recipe('chef-client::service')
+  end
 
 end
