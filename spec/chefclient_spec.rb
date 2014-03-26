ffb9a5d0d0329e0856d03efe72e4ed322bef59cd
Kevin Bridges <kevin@cyberswat.com>
2014-03-25 18:15:48 -0600

    Modifying tests.

---
+++ spec/chefclient_spec.rb
@@ -0,0 +1,21 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/usr/bin'
+  end
+end
+
+describe service('chef-client') do
+  it { should be_enabled }
+end
+
+describe file('/etc/chef/client.rb') do
+  it { should be_file }
+  its(:content) { should match 'chef_server_url' }
+  its(:content) { should match 'validation_client_name "chef-validator"' }
+  its(:content) { should match 'node_name "chefclient-ubuntu-1204"' }
+end
