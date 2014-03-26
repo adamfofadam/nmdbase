e8f6947eeaa18538e9292fff51275bb5ea78d954
Kevin Bridges <kevin@cyberswat.com>
2014-03-25 19:23:03 -0600

    MOre test modifications.

--- spec/chefclient_spec.rb
+++ spec/chefclient_spec.rb
@@ -7,4 +7,9 @@ describe "base::default" do
   it "logs a sample message" do
     expect(chef_run).to write_log "replace this with a meaningful resource"
   end
+
+  expect(chef_run).to include_recipe('chef-client::delete_validation')
+  expect(chef_run).to include_recipe('chef-client::config')
+  expect(chef_run).to include_recipe('chef-client::service')
+
 end
