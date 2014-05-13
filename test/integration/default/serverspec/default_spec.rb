244bfe1cc9b1c54bbee0a9aa137e21bb66912add
Kevin Bridges <kevin@cyberswat.com>
2014-05-13 14:09:03 -0600

    Fixing rubocop issues comitted by other devs.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -18,5 +18,7 @@ describe file('/etc/chef/client.rb') do
   it { should be_file }
   its(:content) { should match 'chef_server_url' }
   its(:content) { should match 'validation_client_name "chef-validator"' }
-  its(:content) { should match /node_name "default-ubuntu-(1204|1404)-(vmware|virtualbox)"/ }
+  its(:content) do
+    should match(/node_name "default-ubuntu-(1204|1404)-(vmware|virtualbox)"/)
+  end
 end
