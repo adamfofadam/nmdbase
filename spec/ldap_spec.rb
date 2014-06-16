2828d661caeb21cb4dc8ecbfafe5360058bafd29
David Arnold <david@newmediadenver.com>
2014-06-16 09:23:38 -0600

    stub'ed in sssd_ldap databag, tmp comment out fail2ban config

--- spec/ldap_spec.rb
+++ spec/ldap_spec.rb
@@ -17,6 +17,44 @@ describe 'nmdbase::ldap' do
         ],
         'secret' => 'test_ldap_secret' }
       )
+    stub_data_bag_item('nmdbase', 'sssd_ldap').and_return(
+      'id' => 'sssd_ldap',
+      '_default' => {
+        'conf' =>  [
+          '[sssd]',
+          'config_file_version = 2',
+          'services = nss, pam',
+          'domains = default',
+          '[nss]',
+          'filter_users = root,ldap,named,avahi,haldaemon,dbus,radiusd,news,nscd',
+          '[pam]',
+          '[domain/default]',
+          'ldap_schema = rfc2307bis',
+          'ldap_user_fullname = displayName',
+          'ldap_user_search_base = ou=people,dc=example,dc=com',
+          'ldap_group_search_base = ou=groups,dc=example,dc=com',
+          'ldap_group_member = member',
+          'ldap_group_nesting_level = 4',
+          'ldap_tls_reqcert = never',
+          'auth_provider = ldap',
+          'ldap_schema = rfc2307bis',
+          'krb5_realm = EXAMPLE.COM',
+          'ldap_search_base = dc=example,dc=com',
+          'ldap_group_member = uniquemember',
+          'id_provider = ldap',
+          'ldap_id_use_start_tls = True',
+          'chpass_provider = ldap',
+          'ldap_uri = ldaps://ldap.example.com',
+          'krb5_kdcip = kerberos.example.com',
+          'cache_credentials = True',
+          'ldap_tls_cacertdir = /etc/openldap/cacerts',
+          'entry_cache_timeout = 600',
+          'ldap_network_timeout = 3',
+          'krb5_realm = EXAMPLE.COM',
+          'krb5_server = kerberos.example.com'
+        ]
+      }
+    )
   end
 
   it 'Installs the LDAP package to set this instance up as a client.' do
