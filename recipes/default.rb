9c3cd4d429953a55b05afbcefdcd94ced2de130b
Kevin Bridges <kevin@cyberswat.com>
2015-04-13 15:46:00 -0600

    Don't like the idea of removing yubico

--- recipes/default.rb
+++ recipes/default.rb
@@ -31,6 +31,7 @@ end
 
 # include_recipe 'fail2ban'
 include_recipe 'nmdbase::ldap'
+include_recipe 'nmdbase::yubico'
 include_recipe 'chef-client::config'
 include_recipe 'chef-client::service'
 include_recipe 'logwatch'
